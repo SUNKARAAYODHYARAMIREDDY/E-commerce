0b78ac4d67532f6711e1acf64ebe1b1072519f8b
SUNKARAAYODHYARAMIREDDY <68509763+SUNKARAAYODHYARAMIREDDY@users.noreply.github.com>
2022-05-10 17:52:01 +0530

    Create exception_handling.java

---
+++ exception_handling.java
@@ -0,0 +1,26 @@
+//We'll need an exception handler for our API in order to properly deal with eventual exceptions it will be helpfull when we go further.
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    @SuppressWarnings("rawtypes")
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorResponse> handle(ConstraintViolationException e) {
+        ErrorResponse errors = new ErrorResponse();
+        for (ConstraintViolation violation : e.getConstraintViolations()) {
+            ErrorItem error = new ErrorItem();
+            error.setCode(violation.getMessageTemplate());
+            error.setMessage(violation.getMessage());
+            errors.addError(error);
+        }
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @SuppressWarnings("rawtypes")
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorItem> handle(ResourceNotFoundException e) {
+        ErrorItem error = new ErrorItem();
+        error.setMessage(e.getMessage());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+}
