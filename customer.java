4f66a877ab1e291291bda338355686e68444f885
SUNKARAAYODHYARAMIREDDY <68509763+SUNKARAAYODHYARAMIREDDY@users.noreply.github.com>
2022-05-10 18:18:58 +0530

    Update customer.java

--- customer.java
+++ customer.java
@@ -1,1 +1,23 @@
+@Entity
+public class Purchase {
+//Identity will generate unique keys
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cust_id;
+  //We are adding some constraints like product name is not null
+    @NotNull(message = "Customer name is required.")
+    @Basic(optional = false)
+    private String name;
 
+   @NotNull(message = "Email is required.")
+    @Basic(optional = false)
+    private String email;
+  
+  @NotNull(message = "Phone number is required.")
+    @Basic(optional = false)
+    private long phno;
+  
+   private String address;
+    // all arguments contructor 
+    // standard getters and setters
+}
